bda938ce87458a6f2815bc5fe67edbbfaeef01a1
mario-krueger <mario-krueger@users.noreply.github.com>
2022-11-02 19:39:21 +0000

    deploy: 8e83926072f9bdeb97356cf012e64793637c46e1

--- navtreedata.js
+++ navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "PlottingFramework", "index.html", [
+  [ "SciRooPlot", "index.html", [
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
