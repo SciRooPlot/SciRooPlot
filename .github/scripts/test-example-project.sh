153d58ef1e9b443e216adcdf6c4104efe9d00834
Mario Krüger <mario.kruger@cern.ch>
2026-08-03 00:26:01 +0200

    add integration test

---
+++ .github/scripts/test-example-project.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+if [ -z "${SRP_TEST_INSTALL:-}" ]; then
+    echo "SRP_TEST_INSTALL is not set"
+    exit 1
+fi
+
+source "${SRP_TEST_INSTALL}/share/scirooplot/scirooplot-env.sh"
+
+WORKDIR=$(mktemp -d)
+trap 'rm -rf "${WORKDIR}"' EXIT
+
+echo "Testing in ${WORKDIR}"
+cd "${WORKDIR}"
+
+echo "=== Testing C++ ==="
+# initialize a project and make a plot with cpp user code
+srp init-cpp TestProject_CPP
+plot examples ptSpec pdf
+test -s TestProject_CPP/output/examples/ptSpec.pdf
+
+srp reset
+
+echo "=== Testing Python ==="
+# initialize a project and make a plot with python user code
+srp init-py TestProject_PY
+plot examples ptSpec pdf
+test -s TestProject_PY/output/examples/ptSpec.pdf
+
+
+echo "SciRooPlot integration test successful"
