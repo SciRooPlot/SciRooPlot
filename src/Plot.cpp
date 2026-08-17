b5f095c68c697b5c9bd79aa11d28383f1b01fdec
Mario Krüger <mario.kruger@cern.ch>
2026-08-17 22:45:10 +0200

    fix urls

--- src/Plot.cpp
+++ src/Plot.cpp
@@ -12,7 +12,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * The GNU General Public License can be found here: <http://www.gnu.org/licenses/>.
+ * The GNU General Public License can be found here: <https://www.gnu.org/licenses/>.
  ******************************************************************************************
  */
 
