5c3f48bffdd081a00f6830dd63c1f561b8595eda
Mario Krüger <mario.kruger@cern.ch>
2026-09-15 22:16:58 +0200

    remove linter and clang-format scripts

--- scripts/linter_check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-LINTER_CMD="cpplint --filter=-whitespace/indent_namespace,-build/c++17,-whitespace/line_length,-whitespace/braces,-readability/todo,-legal/copyright"
-
-${LINTER_CMD} ../include/SciRooPlot/*.h
-${LINTER_CMD} ../src/*.cpp
-${LINTER_CMD} ../app/*.cpp
-${LINTER_CMD} ../user/*.cpp
